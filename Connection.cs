ed0192c181fd5b17abf4e8f4ff032aecfc7a524e
Nico GT <nicogallardo2010@gmail.com>
2025-06-26 19:26:21 -0300

    cambios que hicimos por primera vez

---
+++ Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Peluqueria
+{
+    public class Conexion
+    {
+        private const string servidor = "datasource=127.0.0.1";
+        private const string puerto = "port=3306";
+        private const string username = "username = ";
+        private const string password = "password=";
+        private const string bd = "database=peluqueria";
+
+        public Conexion() //El constructor queda vacío.
+        { //El compilador se encarga de implementarlo
+
+        } //si no lo hacemos nosotros
+        public MySqlConnection getConexion()
+        {
+
+            string cadenaConexion = servidor + ";" + puerto + ";" +
+            username + ";" + password + ";" + bd;
+            return new MySqlConnection(cadenaConexion);
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peluqueria
+{
+    public class Usuario
+    {
+        private int id;
+        private string user;
+        private string password;
+        private string passwordConfirma;
+        private string nombre;
+        private int idTipo;
+
+        public int Id { get => id; set => id = value; }
+        public string User { get => user; set => user = value; }
+        public string Password { get => password; set => password = value; }
+        public string PasswordConfirma { get => passwordConfirma; set => passwordConfirma = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int IdTipo { get => idTipo; set => idTipo = value; }
+        public string Usuarios { get; internal set; }
+    }
+}
